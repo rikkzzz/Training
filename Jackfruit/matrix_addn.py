ccd07034b7062d7b6850d2b98f16d1f88127f1fa
rikkzzz <rithusc3@gmail.com>
2025-07-13 21:24:17 +0530

    Matrix_addn.py
    Imagine you are helping a student with their mathematics homework which involves a lot of problems based on matrix addition. You decide to write a program to automate the task of adding two matrices, which would make the homework a breeze!
    
    Write a program to add two matrices. The program should:
    
    Prompt the user to enter the dimensions of the matrices (assume both matrices have the same dimensions).
    
    Accept the elements of the two matrices from the user.
    
    Display the two matrices.
    
    Add the two matrices.
    
    Print the resultant matrix.
    
    Kindly check the sample test case for input and output format.
    
    Input Format
    
    2 2
    1 2
    3 4
    5 6
    7 8
    
    Constraints
    
    NA
    
    Output Format
    
    First Matrix:
    1 2
    3 4
    Second Matrix:
    5 6
    7 8
    Sum of the two matrices is:
    6 8
    10 12
    
    Sample Input 0
    
    2 2
    1 2
    3 4
    5 6
    7 8
    Sample Output 0
    
    First Matrix:
    1 2 
    3 4 
    Second Matrix:
    5 6 
    7 8 
    Sum of the two matrices is:
    6 8 
    10 12
    Sample Input 1
    
    1 1
    6
    8
    Sample Output 1
    
    First Matrix:
    6
    Second Matrix:
    8
    Sum of the two matrices is:
    14

--- Jackfruit/matrix_addn.py
+++ Jackfruit/matrix_addn.py
@@ -1,1 +1,36 @@
+a,b=map(int,input().split(" "))
+m=[]
+n=[]
+s=[]
+for i in range(a):
+    row=list(map(int,input().split(" ")))
+    m.append(row)
+for i in range(a):
+    row=list(map(int,input().split(" ")))
+    n.append(row)    
+print("First Matrix:")
+for row in m:
+    for v in row:
+        print(v,end=" ")
+    print()    
+
+print("Second Matrix:")
+for row in n:
+    for v in row:
+        print(v,end=" ")
+    print()  
+    
+
+print("Sum of the two matrices is:")                
+for i in range(a):
+    row=[]
+    for j in range(b):
+        row.append(m[i][j]+n[i][j])
+    s.append(row)
+for row in s:
+    for v in row:
+        print(v, end=" ")
+    print()    
+        
+
 
